068f9b5633f2609c5bc07ca289fa33bce460a93c
BluABK <BluABK@users.noreply.github.com>
2019-07-10 23:57:54 +0200

    Removed superflous code and a file

--- src/python3-youtube-api/run.py
+++ src/python3-youtube-api/run.py
@@ -85,43 +85,7 @@ def cli(no_gui, test_channels, update_watch_prio, set_watched_day, refresh_and_p
             else:
                 print(vid)
 
-    if no_gui:
-        run_with_cli()
-    else:
-        if LEGACY_EXCEPTION_HANDLER:
-            """
-            PyQT raises and catches exceptions, but doesn't pass them along. 
-            Instead it just exits with a status of 1 to show an exception was caught. 
-            """
-            # Back up the reference to the exceptionhook
-            sys._excepthook = sys.excepthook
-
-            def my_exception_hook(exctype, value, traceback):
-                global exc_id, exceptions
-                # Ignore KeyboardInterrupt so a console python program can exit with Ctrl + C.
-                if issubclass(exctype, KeyboardInterrupt):
-                    sys.__excepthook__(exctype, value, traceback)
-                    return
-
-                # Log the exception with the logger
-                logger.exception("Intercepted Exception #{}".format(exc_id), exc_info=(exctype, value, traceback))
-
-                # Store intercepted exceptions in a reference list of lists
-                exceptions.append([exctype, value, traceback, exc_id])
-
-                # Increment Exception Identifier
-                exc_id += 1
-
-                # Call the normal Exception hook after
-                # noinspection PyProtectedMember
-                sys._excepthook(exctype, value, traceback)
-
-                # sys.exit(1)       # Alternatively, exit
-
-            # Set the exception hook to our wrapping function
-            sys.excepthook = my_exception_hook
-
-        run_with_gui()
+    run_with_cli()
 
 
 if __name__ == '__main__':

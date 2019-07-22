f9f69640c57d2cfb59f6f34691acd5d37f30a726
BluABK <BluABK@users.noreply.github.com>
2019-07-22 22:42:10 +0200

    Made padStringValue test case into sections of the CLI test case.

--- src/cli/tests/test_cli.cpp
+++ src/cli/tests/test_cli.cpp
@@ -2,17 +2,22 @@
 
 #include "cli.hpp"
 
-// Instantiate the CLI class.
-std::shared_ptr<sane::CLI> cli = std::make_shared<sane::CLI>();
+TEST_CASE( "Testing sane::CLI" ) {
+    // Instantiate the CLI class.
+    std::shared_ptr<sane::CLI> cli = std::make_shared<sane::CLI>();
 
-TEST_CASE( "Strings are padded with correct length (pass)", "[multi-file:2]" ) {
-    REQUIRE( cli->padStringValue("Test", 10).length() == 10 );
-}
+    SECTION( "[function] padStringValue" ) {
 
-TEST_CASE( "Strings longer than padding max length aren't padded (pass)", "[multi-file:2]" ) {
-    REQUIRE( cli->padStringValue("Test", 0).length() == std::string("Test").length() );
-    REQUIRE( cli->padStringValue("Test", 1).length() == std::string("Test").length() );
-    REQUIRE( cli->padStringValue("Test", 2).length() == std::string("Test").length() );
-    REQUIRE( cli->padStringValue("Test", 3).length() == std::string("Test").length() );
-    REQUIRE( cli->padStringValue("Test", 4).length() == std::string("Test").length() );
+        SECTION( "Strings are padded with correct length.", "[multi-file:2]" ) {
+            REQUIRE( cli->padStringValue("Test", 10).length() == 10 );
+        }
+
+        SECTION( "Strings longer than padding max length aren't padded.", "[multi-file:2]" ) {
+            REQUIRE( cli->padStringValue("Test", 0).length() == std::string("Test").length() );
+            REQUIRE( cli->padStringValue("Test", 1).length() == std::string("Test").length() );
+            REQUIRE( cli->padStringValue("Test", 2).length() == std::string("Test").length() );
+            REQUIRE( cli->padStringValue("Test", 3).length() == std::string("Test").length() );
+            REQUIRE( cli->padStringValue("Test", 4).length() == std::string("Test").length() );
+        }
+    }
 }

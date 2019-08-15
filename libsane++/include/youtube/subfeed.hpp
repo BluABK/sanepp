d59f477b0c2885eb096ba34f3a7f88cd1fdecf5d
BluABK <BluABK@users.noreply.github.com>
2019-08-15 16:20:51 +0200

    Renamed YoutubeVideoPublishedAtComparator -> sortYoutubeVideoDateDescending
    and added sortYoutubeVideoDateAscending

--- libsane++/include/youtube/subfeed.hpp
+++ libsane++/include/youtube/subfeed.hpp
@@ -7,7 +7,13 @@
 #include <types.hpp>
 
 namespace sane {
-    struct YoutubeVideoPublishedAtComparator {
+    struct sortYoutubeVideoDateDescending {
+        bool operator ()(const std::shared_ptr<YoutubeVideo> &video1, const std::shared_ptr<YoutubeVideo> &video2) {
+            return video1->getPublishedAt().timestampWithMsec > video2->getPublishedAt().timestampWithMsec;
+        }
+    };
+
+    struct sortYoutubeVideoDateAscending {
         bool operator ()(const std::shared_ptr<YoutubeVideo> &video1, const std::shared_ptr<YoutubeVideo> &video2) {
             return video1->getPublishedAt().timestampWithMsec < video2->getPublishedAt().timestampWithMsec;
         }
@@ -22,6 +28,7 @@ namespace sane {
                                                                 const std::map<std::string, std::string> &t_filter,
                                                                 const std::map<std::string, std::string> &t_optParams = std::map<std::string, std::string>());
 
+    // FIXME: list() version, might also need search() if list turns out to be unreliable.
     std::list<std::shared_ptr<YoutubeVideo>> createSubscriptionsFeed(const std::list<std::string> &t_playlists);
 }
 #endif //SANE_SUBFEED_HPP

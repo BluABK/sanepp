afdc34ca7d9ff02c36758979bfc9877d21669139
BluABK <BluABK@users.noreply.github.com>
2019-08-15 17:22:33 +0200

    Made listUploadedVideos request additional info thru videos.list().
    Upside:   Actually useful metadata is obtained.
    Downside: Pretty much doubles amount of time it takes to retrieve the feed.

--- libsane++/src/youtube/subfeed.cpp
+++ libsane++/src/youtube/subfeed.cpp
@@ -28,20 +28,21 @@ namespace sane {
                                                                 const std::map<std::string, std::string> &t_filter,
                                                                 const std::map<std::string, std::string> &t_optParams) {
         std::list<std::shared_ptr<YoutubeVideo>> videos;
-        nlohmann::json jsonData;
+        nlohmann::json playlistItemsJson;
+        nlohmann::json videoListJson;
 
         // Instantiate API Handler
         std::shared_ptr<sane::APIHandler> api = std::make_shared<sane::APIHandler>();
 
         // For playlist in t_playlists
         // TODO: This probably needs threading
-        int counter = 1;
+        int playlistCounter = 1;
         for (const auto& playlist : t_playlists) {
-            std::cout << "Retrieving playlist " << counter << " / " << t_playlists.size() << "\t" << playlist
+            std::cout << "Retrieving playlist " << playlistCounter << " / " << t_playlists.size() << "\t" << playlist
                       << std::endl;
 
-            // YouTube API essentials
-//            const std::string part = t_part;
+            // Create an actual useful part to be used with videos.list().
+            const std::string informativePart = t_part + "," + "snippet";
 
             // Add passed filters and optional parameters.
             std::map<std::string,std::string> filter = t_filter;
@@ -55,38 +56,76 @@ namespace sane {
                 filter.erase("id");
             }
 
-            if (filter.find("id") != filter.end()) {
+            if (filter.find("playlistId") != filter.end()) {
                 std::cerr << "listUploadedVideos ERROR: FILTER playlistId=" << filter["playlistId"]
                           << " was specified but will be overridden with " << playlist << "!" << std::endl;
             }
 
-            // Set the playlist id.
+            // Set the current playlist id.
             filter["playlistId"] = playlist;
 
-            // Make the SAPI request and retrieve JSON.
-            jsonData = api->sapiGetPlaylistItemsList(t_part, filter, optParams);
+            // Make the SAPI request and retrieve (a rather limited) JSON.
+            // NB: Note using t_part (likely only contentDetails), not part.
+            playlistItemsJson = api->sapiGetPlaylistItemsList(t_part, filter, optParams);
 
-            // Make sure the response was valid.
-            if (!jsonData.empty()) {
-                // For video item in response // FIXME: No pagination support, will cutoff at 50 max.
-                for (auto playlistItemJson : jsonData["items"]) {
-                    // Create the YoutubeVideo entity.
-                    std::shared_ptr<YoutubeVideo> video = std::make_shared<YoutubeVideo>(playlistItemJson);
+            // Make sure the playlistItemsJson response was valid.
+            if (!playlistItemsJson.empty()) {
+                // Do some separate videos.list() API request for current playlist items to actually obtain useful info:
 
-                    // Finally append the video to the list.
-                    videos.push_back(video);
-                }
-            }
-            counter++;
-        }
+                // 1. Clear playlistItems-specific filters:
+                filter.erase("playlistId");
+
+                // 2. Populate filter 'id=' with list of video IDs from the playlistItems response.
+                bool firstItem = true;
+                for (const auto& playlistItemJson : playlistItemsJson["items"]) {
+                    // The "id" field in playlistItem is the item's ID, not the video's.
+
+                    // The actual video ID can be found inside of the parts.
+                    if (playlistItemJson.find("contentDetails") != playlistItemJson.end()) {
+                        if (firstItem) {
+                            // Don't prepend comma to first item.
+                            filter["id"] = playlistItemJson["contentDetails"]["videoId"].get<std::string>();
+
+                            firstItem = false;
+                        } else {
+                            // Append id to string, prepended with comma.
+                            filter["id"] += "," + playlistItemJson["contentDetails"]["videoId"].get<std::string>();
+                        }
+                    } else {
+                        // Kind is playlistItem, but no snippet or contentDetails were provided.
+                        std::cerr << "listUploadedVideos Error: Unable to set video ID: Kind is youtube#playlistItem, "
+                                  << "but contentDetails parts was not available!" << std::endl;
+                    } // if contentDetails in playlistItemJson
+                } // for playlistItemJson in current playlistItemsJson
+
+                // 3. Request proper information for the current video IDs using the API's videos.list().
+                std::cout << "\tRetrieving additional video info... " << std::endl;
+                videoListJson = api->sapiGetVideosList(informativePart, filter, optParams);
+
+                // Make sure the videoListJson response was valid.
+                if (!videoListJson.empty()) {
+                    // For video item in response // FIXME: No pagination support, will cutoff at 50 max.
+                    for (auto videoJson : videoListJson["items"]) {
+                        // Create the YoutubeVideo entity.
+                        std::shared_ptr<YoutubeVideo> video = std::make_shared<YoutubeVideo>(videoJson);
+
+                        // Finally append the video to the list.
+                        videos.push_back(video);
+                    } // for video in videoListJson
+                } // if videoListJson not empty
+            } // if playlistItemsJson not empty
+            playlistCounter++;
+        } // for playlist in t_playlists
 
         return videos;
     }
 
     // Create subfeed from a list of channel uploaded videos playlists.
     std::list<std::shared_ptr<YoutubeVideo>> createSubscriptionsFeed(const std::list<std::string> &t_playlists) {
-        // YouTube API essentials
-        const std::string part = "snippet,contentDetails";
+        // YouTube API essentials:
+        // Due to the limited amount of info in youtube#playlistItems we'll just request the part that holds videoId,
+        // and then we'll do a separate videos.list() API request for those IDs further down the line.
+        const std::string part = "contentDetails";
         // Any filter besides "id="
         std::map<std::string,std::string> filter;
         std::map<std::string,std::string> optParams;
